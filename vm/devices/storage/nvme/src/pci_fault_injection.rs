7db4d7d760dc4a761b72f59b51416bff17b12a6f
Guramrit Singh <gurasingh@microsoft.com>
2025-07-22 12:18:53 -0700

    First commit, with only a controller delay available

--- vm/devices/storage/nvme/src/pci_fault_injection.rs
+++ vm/devices/storage/nvme/src/pci_fault_injection.rs
@@ -1,18 +1,11 @@
+use crate::DOORBELL_STRIDE_BITS;
 use crate::NvmeController;
 use crate::NvmeControllerCaps;
 use crate::NvmeControllerClient;
-use crate::PAGE_MASK;
-use crate::namespace::Namespace;
-use crate::queue::CompletionQueue;
-use crate::queue::DoorbellRegister;
-use crate::queue::QueueError;
-use crate::queue::SubmissionQueue;
 use crate::spec;
-use crate::workers::admin::AdminConfig;
-use crate::workers::admin::AdminHandler;
-use crate::workers::admin::AdminState;
 use chipset_device::ChipsetDevice;
 use chipset_device::io::IoError;
+use chipset_device::io::IoError::InvalidRegister;
 use chipset_device::io::IoResult;
 use chipset_device::mmio::MmioIntercept;
 use chipset_device::mmio::RegisterMmioIntercept;
@@ -20,16 +13,11 @@ use chipset_device::pci::PciConfigSpace;
 use guestmem::GuestMemory;
 use inspect::Inspect;
 use inspect::InspectMut;
+use mesh::Cell;
+use pal_async::timer::PolledTimer;
 use pci_core::msi::RegisterMsi;
 use std::any::Any;
-use std::collections::BTreeMap;
-use std::sync::Arc;
-use task_control::AsyncRun;
-use task_control::Cancelled;
-use task_control::InspectTask;
-use task_control::StopTask;
-use task_control::TaskControl;
-use user_driver_emulated_mock::DeviceTestMemory;
+use std::time::Duration;
 use vmcore::device_state::ChangeDeviceState;
 use vmcore::save_restore::SaveError;
 use vmcore::save_restore::SaveRestore;
@@ -56,20 +44,11 @@ pub enum FaultInjectionAction {
 #[derive(InspectMut)]
 pub struct NvmeControllerFaultInjection {
     #[inspect(skip)]
+    driver: VmTaskDriver,
+    #[inspect(skip)]
     inner: NvmeController,
-    /// Fault injection callback for NVMe controller operations
-    #[inspect(skip)]
-    fi: Box<
-        dyn Fn(&str, Vec<Box<dyn Any>>) -> (FaultInjectionAction, Vec<Box<dyn Any>>) + Send + Sync,
-    >,
-    memory_inner: GuestMemory,
-    memory_outer: GuestMemory,
-    driver_source: VmTaskDriver,
-    #[inspect(skip)]
-    doorbells: Vec<Arc<DoorbellRegister>>,
-    regs: Regs,
-    #[inspect(skip)]
-    admin: TaskControl<AdminHandlerFaultInjection, AdminState>,
+    #[inspect(hex, with = "|x| inspect::AsDebug(x.get())")]
+    admin_delay: Cell<Duration>,
 }
 
 #[derive(Inspect)]
@@ -88,42 +67,10 @@ impl NvmeControllerFaultInjection {
         register_msi: &mut dyn RegisterMsi,
         register_mmio: &mut dyn RegisterMmioIntercept,
         caps: NvmeControllerCaps,
-        fi: Box<
-            dyn Fn(&str, Vec<Box<dyn Any>>) -> (FaultInjectionAction, Vec<Box<dyn Any>>)
-                + Send
-                + Sync,
-        >,
-        pages: u64,
+        admin_delay: Cell<Duration>,
     ) -> Self {
-        // Create memory for inner
-        let memory_inner =
-            DeviceTestMemory::new(pages * 2, false, "test_nvme_driver").guest_memory();
-
-        // Deal with Doorbell registers copy
-        let num_qids = 2 + caps.max_io_queues * 2; // Assumes that max_sqs == max_cqs
-        let doorbells: Vec<_> = (0..num_qids)
-            .map(|_| Arc::new(DoorbellRegister::new()))
-            .collect();
-
-        // TODO: Unused, but will probably need to update the AdminConfig -> AdminConfigFaultInjection and not pass this in anymore!
-        let interrupts = vec![];
-
-        let qe_sizes = Arc::new(Default::default());
-        let handler: AdminHandlerFaultInjection = AdminHandlerFaultInjection::new(
-            driver_source.simple(),
-            AdminConfig {
-                driver_source: driver_source.clone(),
-                mem: guest_memory.clone(),
-                interrupts,
-                doorbells: doorbells.clone(),
-                subsystem_id: caps.subsystem_id,
-                max_sqs: caps.max_io_queues,
-                max_cqs: caps.max_io_queues,
-                qe_sizes: Arc::clone(&qe_sizes),
-            },
-        );
-
         Self {
+            driver: driver_source.simple(),
             inner: NvmeController::new(
                 driver_source,
                 guest_memory.clone(),
@@ -131,18 +78,7 @@ impl NvmeControllerFaultInjection {
                 register_mmio,
                 caps,
             ),
-            fi,
-            memory_inner,
-            memory_outer: guest_memory.clone(),
-            driver_source: driver_source.simple(),
-            doorbells,
-            regs: Regs {
-                asq: 0,
-                acq: 0,
-                aqa: spec::Aqa::default(),
-                cc: spec::Cc::default(),
-            },
-            admin: TaskControl::new(handler),
+            admin_delay,
         }
     }
 
@@ -153,80 +89,34 @@ impl NvmeControllerFaultInjection {
 
     /// Reads from the virtual BAR 0.
     pub fn read_bar0(&mut self, addr: u16, data: &mut [u8]) -> IoResult {
-        // FI Input
-        let mut input: Vec<Box<dyn Any>> = vec![Box::new(addr as u32), Box::new(data.to_vec())];
-        let (action, mut responses) = (self.fi)("read_bar0", input);
-
-        // Act on input Fault Injection
-        match action {
-            FaultInjectionAction::Drop => {
-                // Returns an I/O result.
-                // TODO: Since IoResult doesn't implement Copy it cannot be sent from the fault injection function.
-                // This is a limitation of the current design. We can change this in the future.
-                return IoResult::Ok;
-            }
-            _ => {
-                // Not every action needs to be handled everywhere, only the relevant ones.
-                tracing::warn!("FaultInjectionAction {:?} not handled in read_bar0", action);
-            }
-        }
-
-        // Normal Behaviour.
         self.inner.read_bar0(addr, data)
     }
 
     /// Writes to the virtual BAR 0.
     pub fn write_bar0(&mut self, addr: u16, data: &[u8]) -> IoResult {
-        let data_original = data.clone();
-
-        let update_reg = |x: u64| {
-            if data.len() == 8 {
-                u64::from_ne_bytes(data.try_into().unwrap())
-            } else {
-                let data = u32::from_ne_bytes(data.try_into().unwrap()) as u64;
-                if addr & 7 == 0 {
-                    (x & !(u32::MAX as u64)) | data
-                } else {
-                    (x & u32::MAX as u64) | (data << 32)
-                }
+        if addr >= 0x1000 {
+            // Doorbell write.
+            let base = addr - 0x1000;
+            let index = base >> DOORBELL_STRIDE_BITS;
+            if (index << DOORBELL_STRIDE_BITS) != base {
+                return IoResult::Err(InvalidRegister);
             }
-        };
-
-        // Intercept and duplicate admin queue setup!
-        let handled = match spec::Register(addr & !7) {
-            spec::Register::ASQ => {
-                if !self.regs.cc.en() {
-                    self.regs.asq = update_reg(self.regs.asq) & PAGE_MASK;
-                } else {
-                    tracelimit::warn_ratelimited!("attempt to set asq while enabled");
-                }
-                true
+            let Ok(data) = data.try_into() else {
+                return IoResult::Err(IoError::InvalidAccessSize);
+            };
+            let _ = u32::from_ne_bytes(data);
+            // Delay only the Admin Submission Queue doorbell writes.
+            if index == 0 {
+                async {
+                    PolledTimer::new(&self.driver)
+                        .sleep(self.admin_delay.get())
+                        .await
+                };
             }
-            spec::Register::ACQ => {
-                if !self.regs.cc.en() {
-                    self.regs.acq = update_reg(self.regs.acq) & PAGE_MASK;
-                } else {
-                    tracelimit::warn_ratelimited!("attempt to set acq while enabled");
-                }
-                true
-            }
-            _ => false,
-        };
-
-        let Ok(data) = data.try_into() else {
-            return IoResult::Err(IoError::InvalidAccessSize);
-        };
-        let data = u32::from_ne_bytes(data);
-
-        // Handle 32-bit registers.
-        match spec::Register(addr) {
-            spec::Register::CC => self.set_cc(data.into()),
-            spec::Register::AQA => self.regs.aqa = data.into(),
-            _ => {}
         }
 
         // Handled all queue related jargon, let the inner controller handle the rest
-        self.inner.write_bar0(addr, data_original)
+        self.inner.write_bar0(addr, data)
     }
 
     pub fn fatal_error(&mut self) {
@@ -304,58 +194,5 @@ impl SaveRestore for NvmeControllerFaultInjection {
         state: Self::SavedState,
     ) -> Result<(), vmcore::save_restore::RestoreError> {
         self.inner.restore(state)
-    }
-}
-
-/// An admin handler shim layer for fault injection.
-#[derive(Inspect)]
-pub struct AdminHandlerFaultInjection {
-    driver: VmTaskDriver,
-    config: AdminConfig,
-}
-
-impl AdminHandlerFaultInjection {
-    pub fn new(driver: VmTaskDriver, config: AdminConfig) -> Self {
-        Self { driver, config }
-    }
-}
-
-impl AsyncRun<AdminState> for AdminHandlerFaultInjection {
-    async fn run(
-        &mut self,
-        stop: &mut StopTask<'_>,
-        state: &mut AdminState,
-    ) -> Result<(), Cancelled> {
-        loop {
-            let event = stop.until_stopped(self.next_event(state)).await?;
-            tracing::debug!(
-                "THIS IS YOUR CAPTAIN SPEAKING: admin handler is intercepting, I repeat, admin handler is intercepting"
-            );
-        }
-        Ok(())
-    }
-}
-
-impl AdminHandlerFaultInjection {
-    async fn next_event(&mut self, state: &mut AdminState) {
-        let _ = state.admin_cq.wait_ready(&self.config.mem);
-    }
-}
-
-impl InspectTask<AdminState> for AdminHandlerFaultInjection {
-    fn inspect(&self, req: inspect::Request<'_>, state: Option<&AdminState>) {
-        req.respond().merge(self).merge(state);
-    }
-}
-
-pub struct AdminStateFaultInjection {
-    admin_sq: SubmissionQueue,
-}
-
-impl AdminStateFaultInjection {
-    pub fn new(asq: u64, asqs: u16, acq: u64, acqs: u16) -> Self {
-        Self {
-            admin_sq: SubmissionQueue::new(handler.config.doorbells[0].clone(), asq, asqs, None),
-        }
     }
 }
